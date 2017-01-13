9a647a79b30c70cfb04c7aa4109f0aef5f4f7caa
tyrone mcelrath <tyrone mcelrath>
2017-01-13 14:30:59 -0500

    change styled,added content

---
+++ show.js
@@ -0,0 +1,19 @@
+
+function frame(etc){
+    
+switch(etc){
+    case 1:
+            document.getElementById("highlight").innerHTML="<h2>UWP Calculator</h2><p>I'm trying to become more familiar with UWP so im designing simple apps in visual studio in my passtime.</p>";
+            break;
+    case 2:
+            document.getElementById("highlight").innerHTML="<h2>To-Do list Html App</h2><p>Ive been working with Phone Gap recently</p><p>and just overall enjoyed the Experience.</p>";
+            break;
+    case 3:
+            document.getElementById("highlight").innerHTML="<h2>Drawing App</h2><p>A bigger,slow project of mine where hopefully I can build a fully funcctioning draw app where you can import and export photos.</p>";
+            break;
+    case 4:
+            document.getElementById("highlight").innerHTML="<h2>Ping Pong/Connect 4</h2><p>I Want to be more Comfortable with canvas and SVG</p><p>so I have been working on some fun little distractions to become more familiar.</p>";
+            break;
+}
+}
+
