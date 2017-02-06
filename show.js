12f1895f8d3309e485d0e419eb72bd3c3b15b5b2
tyrone mcelrath <tyrone mcelrath>
2017-02-06 13:51:37 -0500

    change front head

--- show.js
+++ show.js
@@ -6,7 +6,7 @@ switch(etc){
             document.getElementById("highlight").innerHTML="<h2>UWP Calculator</h2><p>I'm trying to become more familiar with UWP so im designing simple apps in visual studio in my passtime.</p>";
             break;
     case 2:
-            document.getElementById("highlight").innerHTML="<h2>To-Do list Html App</h2><p>Ive been working with Phone Gap recently</p><p>and just overall enjoyed the Experience.</p>";
+            document.getElementById("highlight").innerHTML="<h2>To-Do list Html App</h2><p>Ive been working with Phone Gap recently</p><p>and just overall enjoyed the Experience,and wanted to work on somthing new.</p>";
             break;
     case 3:
             document.getElementById("highlight").innerHTML="<h2>Drawing App</h2><p>A bigger,slow project of mine where hopefully I can build a fully funcctioning draw app where you can import and export photos.</p>";
