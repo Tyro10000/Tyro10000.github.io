d42e87d29b5becccb4322ab87d0e520cc09351cb
tyrone mcelrath <tyrone mcelrath>
2017-02-21 19:26:07 -0500

    Added projects slide,changed buttons

--- dash.js
+++ dash.js
@@ -4,6 +4,21 @@ function expand(etc) {
 
 }
 
+function expunge() {
+    $("#1").slideUp();
+    $("#2").slideUp();
+    $("#3").slideUp();
+    $("#4").slideUp();
+    $("#point1").slideUp();
+    $("#point2").slideUp();
+    $("#point3").slideUp();
+    $("#point4").slideUp();
+
+}
+
+function show() {
+    $(".projects_extend").slideToggle();
+}
 var pick = 1;
 
 function flip(etx) {
@@ -13,4 +28,20 @@ function flip(etx) {
     if (pick < 1)
         pick = 4;
     $("#image_holder").html('<img src="pics/pik' + pick + '.png " class="image_current " />');
+    switch (pick) {
+        case 1:
+            document.getElementById("img_sum").innerHTML = "Connect-4";
+            break;
+        case 2:
+            document.getElementById("img_sum").innerHTML = "Connect-4";
+            break;
+        case 3:
+            document.getElementById("img_sum").innerHTML = "Hotel Class Project";
+            break;
+        case 4:
+            document.getElementById("img_sum").innerHTML = "Hotel Class Project";
+            break;
+
+
+    }
 }
